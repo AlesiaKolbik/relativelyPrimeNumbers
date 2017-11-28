7e70451e633956202d4806521e2d44a4c9a54b56
Alesia Kolbik <moyaalesia@mail.ru>
2017-11-28 21:48:14 +0300

    refactor the code

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,22 +5,25 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Введите натуральное число:");
-        int input=scanner.nextInt();
-        int counter=0;   //счетчик колличества выведенных натуральных чисел
-        while (input!=counter){
-            for(int i = 2; ; i=i+2 ){
-                    if(relativelyPrimeNumbers(i,i+1))  {
-                        System.out.println(i+" "+(i+1));
-                        counter++;
-                        if(counter==input){
-                            break;
-                        }
+        int input = scanner.nextInt();
+        int counter = 0;   //счетчик колличества выведенных натуральных чисел
+        for (int i = 2; ; i++) {
+            for (int j = 3; ; j++) {
+                if (relativelyPrimeNumbers(i, j)) {
+                    System.out.println(i + " " + j);
+                    counter++;
+                    if (counter == input) {
+                        break;
                     }
                 }
             }
+            if (counter == input) {
+                break;
+            }
         }
+    }
     public static boolean relativelyPrimeNumbers(int numberFirst, int numberSecond){
         boolean relative=true;
         int maxOfNumbers=Math.max(numberFirst,numberSecond);
